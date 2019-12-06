7d08935d85a69d517929b93995aacb05ab052058
Victor Atterström <victor.atterstrom@se.ibm.com>
2019-12-06 13:07:21 +0100

    Update app.js
    hugo

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ if (process.env.PORT) {
 
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Welcome to IBM Cloud DevOps using containers. Let\'s go use the Continuous Delivery Service');
+  res.send('Nice dator Hugo! :)');
 });
 
 app.listen(PORT);
